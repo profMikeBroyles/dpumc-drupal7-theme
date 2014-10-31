91573696bd53e8c4e4b0e4fb8f25e232c8f62fa8
Prof Mike Broyles <profmikebroyles@gmail.com>
2014-10-30 21:31:39 -0400

    Initial fork for DPUMC theme

---
+++ template.php
@@ -0,0 +1,30 @@
+<?php
+function dpumc_page_class($sidebar_first, $sidebar_second) {
+	if ($sidebar_first && $sidebar_second) {
+		$id = 'layout-type-2';	
+	}
+	else if ($sidebar_first || $sidebar_second) {
+		$id = 'layout-type-1';
+	}
+
+	if(isset($id)) {
+		print ' id="'. $id .'"';
+	}
+}
+
+function dpumc_preprocess_html(&$vars) {
+  // Add conditional CSS for IE6.
+drupal_add_css(path_to_theme() . '/style.ie6.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'IE 6', '!IE' => FALSE), 'preprocess' => FALSE));
+}
+
+function dpumc_preprocess_maintenance_page(&$variables) {
+  if (!$variables['db_is_active']) {
+    unset($variables['site_name']);
+  }
+  drupal_add_css(drupal_get_path('theme', 'dpumc') . '/maintenance.css');
+  drupal_add_js(drupal_get_path('theme', 'dpumc') . '/scripts/jquery.cycle.all.js');
+}
+
+if (drupal_is_front_page()) {
+  drupal_add_js(drupal_get_path('theme', 'dpumc') . '/scripts/jquery.cycle.all.js');
+}
